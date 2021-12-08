09adc1c9c3b08aa4b820647467ed8c9a5f18471f
KRdoncc <cow1730@naver.com>
2021-12-08 15:18:05 +0900

    VSC로 옮겨가기 위한 푸쉬.

--- src/main/java/com/example/demo/RedsocksProjectApplication.java
+++ src/main/java/com/example/demo/RedsocksProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages=("web"))
 public class RedsocksProjectApplication {
 
 	public static void main(String[] args) {
