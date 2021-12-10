1e22607e302871c4601b11d6bd88d1ba9eab8de6
saehyen <rlatpgus4564@naver.com>
2021-12-10 11:41:34 +0900

    게시판 CRUD 제작

---
+++ src/main/java/com/board/study/dto/board/BoardResponseDto.java
@@ -0,0 +1,35 @@
+package com.board.study.dto.board;
+
+import com.board.study.entity.board.Board;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+//게시판 응답 데이터를 담당
+import java.time.LocalDateTime;
+import com.board.study.entity.board.Board;
+import lombok.Getter;
+
+@Getter
+public class BoardResponseDto {
+	private Long id;
+	private String title;
+	private String content;
+	private int readCnt;
+	private String registerId;
+	private LocalDateTime registerTime;
+
+	public BoardResponseDto(Board entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.content = entity.getContent();
+		this.readCnt = entity.getReadCnt();
+		this.registerId = entity.getRegisterId();
+		this.registerTime = entity.getRegisterTime();
+	}
+
+	@Override
+	public String toString() {
+		return "BoardListDto [id=" + id + ", title=" + title + ", content=" + content + ", readCnt=" + readCnt
+				+ ", registerId=" + registerId + ", registerTime=" + registerTime + "]";
+	}
+}
---
+++ src/main/java/com/board/study/web/BoardController.java
@@ -0,0 +1,68 @@
+package com.board.study.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.board.study.dto.board.BoardRequestDto;
+import com.board.study.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class BoardController {
+	
+	private final BoardService boardService;
+	
+	@GetMapping("/board/list")
+	public String getBoardListPage(Model model
+			, @RequestParam(required = false, defaultValue = "0") Integer page
+			, @RequestParam(required = false, defaultValue = "5") Integer size) throws Exception {
+		
+		try {
+			model.addAttribute("resultMap", boardService.findAll(page, size));
+		} catch (Exception e) {
+			throw new Exception(e.getMessage()); 
+		}
+		
+		return "/board/list";
+	}
+	
+	@GetMapping("/board/write")
+	public String getBoardWritePage(Model model, BoardRequestDto boardRequestDto) {
+		return "/board/write";
+	}
+	
+	@GetMapping("/board/view")
+	public String getBoardViewPage(Model model, BoardRequestDto boardRequestDto) throws Exception {
+		
+		try {
+			if(boardRequestDto.getId() != null) {
+				model.addAttribute("info", boardService.findById(boardRequestDto.getId()));
+			}
+		} catch (Exception e) {
+			throw new Exception(e.getMessage()); 
+		}
+		
+		return "/board/view";
+	}
+	
+	@PostMapping("/board/write/action")
+	public String boardWriteAction(Model model, BoardRequestDto boardRequestDto) throws Exception {
+		
+		try {
+			Long result = boardService.save(boardRequestDto);
+			
+			if (result < 0) {
+				throw new Exception("#Exception boardWriteAction!");
+			}
+		} catch (Exception e) {
+			throw new Exception(e.getMessage()); 
+		}
+		
+		return "redirect:/board/list";
+	}
+}
