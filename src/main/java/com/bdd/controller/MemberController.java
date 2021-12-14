033a2f7386cbd0e69f93e263b2dc77b6fce06422
saehyen <rlatpgus4564@naver.com>
2021-12-14 16:58:57 +0900

    백엔드 복구

---
+++ src/main/java/com/bdd/controller/MemberController.java
@@ -0,0 +1,67 @@
+package com.bdd.controller;
+
+import com.bdd.dto.MemberDto;
+import com.bdd.service.MemberService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@AllArgsConstructor
+public class MemberController {
+    private MemberService memberService;
+
+
+
+    // 회원가입 페이지
+    @GetMapping("/user/signup")
+    public String dispSignup() {
+        return "/page/signup";
+    }
+
+    // 회원가입 처리
+    @PostMapping("/user/signup")
+    public String execSignup(MemberDto memberDto) {
+        memberService.joinUser(memberDto);
+
+        return "redirect:/user/login";
+    }
+
+    // 로그인 페이지
+    @GetMapping("/user/login")
+    public String dispLogin() {
+        return "/page/login";
+    }
+
+//    // 접근 거부 페이지
+//    @GetMapping("/user/denied")
+//    public String dispDenied() {
+//        return "/denied";
+//    }
+
+    // 내 정보 페이지
+    @GetMapping("/user/info")
+    public String dispMyInfo() {
+        return "/page/profile";
+    }
+    
+    @GetMapping("/user/login/result")
+    public String dispLoginResult() {
+        return "/page/loginSuccess";
+    }
+    
+    @GetMapping("/user/logout/result")
+    public String dispLogoutResult() {
+        return "/page/logout";
+    }
+
+//    // 어드민 페이지
+//    @GetMapping("/admin")
+//    public String dispAdmin() {
+//        return "/admin";
+//    }
+    
+    
+    
+}
---
+++ src/main/java/com/bdd/domain/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.bdd.domain.repository;
+
+import com.bdd.domain.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Integer> {
+    
+	Optional<MemberEntity> findById(String userId);
+}
