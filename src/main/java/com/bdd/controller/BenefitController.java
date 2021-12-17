72ac1387a98e0f4ede84378311ca407cfbaebae9
matjgus <matjgus@gmail.com>
2021-12-17 20:32:21 +0900

    Benefit 추가

---
+++ src/main/java/com/bdd/controller/BenefitController.java
@@ -0,0 +1,41 @@
+package com.bdd.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bdd.dto.board.BenefitRequestDto;
+import com.bdd.dto.board.BenefitResponseDto;
+import com.bdd.service.BenefitService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class BenefitController {
+	private final BenefitService benefitService;
+	
+	@GetMapping("/benefitlist")
+	public List<BenefitResponseDto> getbenefitList(){
+		return benefitService.findAll();
+	}
+	@PostMapping("/benefit")
+	public Integer reqBenefit(@RequestBody BenefitRequestDto requestDto) {
+		return benefitService.save(requestDto);
+	}
+	@PostMapping("/benefitlist")
+	public List<Integer> reqDeedList(@RequestBody List<BenefitRequestDto> requestDtos){
+		BenefitRequestDto requestDto;
+		List<Integer> deedList = new ArrayList<Integer>();
+		
+		for(int i=0; i<requestDtos.size();i++) {
+			requestDto = requestDtos.get(i);
+			deedList.add(benefitService.save(requestDto));
+		}
+		return deedList;
+	} 
+}
---
+++ src/main/java/com/bdd/dto/board/BenefitRequestDto.java
@@ -0,0 +1,26 @@
+package com.bdd.dto.board;
+
+import com.bdd.domain.entity.board.Benefit;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BenefitRequestDto {
+	private int uid;
+	private char is_get10;
+	private char is_get20;
+	private char is_get30;
+	
+	public Benefit toEntity() {
+		return Benefit.builder()
+				.uid(uid)
+				.is_get10(is_get10)
+				.is_get20(is_get20)
+				.is_get30(is_get30)
+				.build();
+	}
+}
