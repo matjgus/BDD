dee7753cda832ae4ed152a34c7972b39b562f228
KRdoncc <cow1730@naver.com>
2021-12-21 15:21:28 +0900

    benefit BAck yEah~

--- src/main/java/com/bdd/domain/entity/board/BenefitRepository.java
+++ src/main/java/com/bdd/domain/entity/board/BenefitRepository.java
@@ -1,9 +1,12 @@
 package com.bdd.domain.entity.board;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BenefitRepository extends JpaRepository<Benefit, Integer> {
 	List<Benefit> findByid(String id);
+
+	Optional<Benefit> findById(String id);
 }
